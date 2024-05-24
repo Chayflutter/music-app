bcd8aba088f0c88dc148329e1bd71aa08f870f52
geraltguru <geraltguru@gmail.com>
2024-05-24 08:24:05 +0000

    song page2

--- lib/components/neu_box.dart
+++ lib/components/neu_box.dart
@@ -1,1 +1,11 @@
 import 'package:flutter/material.dart';
+class NeuBox extends StatelessWidget {
+  const NeuBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
